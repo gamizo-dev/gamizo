722d7cc6f4364965957f3dc9dfdb8d69c38d235f
ggsingla <ggsingla777@gmail.com>
2021-06-06 15:23:33 +0530

    added menu

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 var hamburger = document.getElementById("hamburger");
-
+var menu = document.getElementById("menu");
 var line = document.getElementsByClassName("line");
 
 var i = 0;
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("menu-open");
+  menu.classList.toggle("menu-open");
   for (i = 0; i < line.length; i++) {
     line[i].classList.toggle("menu-open");
   }
