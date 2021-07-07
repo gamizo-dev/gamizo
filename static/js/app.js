3f3de20e5e75ee9808a1502e49bea32406a06618
Adityagupta1625 <guptaaditya2512@gmail.com>
2021-07-07 13:23:48 +0530

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,64 @@ hamburger.addEventListener("click", function () {
   }
 });
 
+var y = document.getElementsByClassName("report_form");
+var op =0;
+function toggleText(id){
+  op++;
+  console.log("toggleText");
+  var x = document.getElementById("r"+id);
+  if (x.style.display != "block") {
+    y[id-1].style.display="block";
+    for( i=0;i<y.length;i++){
+      if(i!==id-1){
+      y[i].style.display="none";
+      }
+    }
+  }
+
+}
+
+// window.onload = () => {
+//   new Vue({
+//     el: "#app",
+//     data() {
+//       return {
+//         sortBy: "age",
+//         sortDesc: false,
+//         fields: [
+          
+//           { key: "first_name", sortable: true },
+//           { key: "age", sortable: true },
+//           { key: "isActive", sortable: false }
+//         ],
+//         items: [
+//           {
+//             isActive: true,
+//             age: 40,
+//             first_name: "Dickerson",
+//             last_name: "Macdonald"
+//           },
+//           { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
+//           {
+//             isActive: false,
+//             age: 89,
+//             first_name: "Geneva",
+//             last_name: "Wilson"
+//           },
+//           { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" }
+//         ]
+//       };
+//     }
+//   });
+// };
+
+function fnc(){
+    if(op%2==0){
+      console.log("body");
+      for( i=0;i<y.length;i++){
+        if(y[i].style.display="block"){
+          y[i].style.display="none";
+        }
+      }
+    }
+  }
