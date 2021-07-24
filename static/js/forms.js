3e9f2376660bb414fbeaee2b227212c1e73d00ce
Ravinder Chadha <ravinderchadha1233@gmail.com>
2021-07-24 19:01:17 +0530

    share form open close fixed

--- static/js/forms.js
+++ static/js/forms.js
@@ -4,10 +4,9 @@ var cancel= document.getElementsByClassName("cancel");
 var closebtn= document.getElementsByClassName("closebtn");
 var report = document.getElementById("reportfrm");
 var share = document.getElementById("sharefrm");
-var sharebtn = document.getElementsByClassName("share");
+var sharebtn = document.getElementsByClassName("btn share");
 var j=0;
 function ShowReportfrm(id, cat) {
-    console.log(j);
     gamename=id;
     category=cat;
     if(report.style.display = "none"){
@@ -30,23 +29,22 @@ cancel.addEventListener("click", function(event){
     event.preventDefault()
   });
 
-function ShowSharefrm(id, cat) {
-    console.log(j);
-    gamename=id;
-    category=cat;
+function ShowSharefrm() {
+    // gamename=id;
+    // category=cat;
     if(share.style.display = "none"){
       share.style.display = "block";
-    j=1;
+    k=1;
     }
-    document.getElementById('repname').value=gamename;
-    document.getElementById('repname2').value=category;
+    // document.getElementById('repname').value=gamename;
+    // document.getElementById('repname2').value=category;
 
 }
 function HideSharefrm(){
-    if(j!==1){
-        if(report.style.display="block"){
-            report.style.display="none";
+    if(k!==1){
+        if(share.style.display="block"){
+            share.style.display="none";
         }
     }
-    j=0;
+    k=0;
 }
