2a64981924ce9663bb4798cf21e3d840a36da64b
ggsingla <ggsingla777@gmail.com>
2021-08-07 15:29:33 +0530

    Google Sign up button in the menu

--- static/js/video.js
+++ static/js/video.js
@@ -1,31 +1,29 @@
 //videoplayer js
 const videos = document.getElementsByClassName("impvideo");
-const play_arr = DocumentTimeline.getElementsByClassName("play_arrow");
+const play_btn = DocumentTimeline.getElementsByClassName("play_arrow");
 // const backvideos= document.getElementsByClassName("backdrop-video");
-function newfunc(){
-for(var i=0;i<videos.length;i++){
-      elementInViewport(videos[i]);
-}
+function newfunc() {
+  for (var i = 0; i < videos.length; i++) {
+    elementInViewport(videos[i]);
+  }
 }
 function elementInViewport(videos) {
+  var bounding = videos.getBoundingClientRect();
 
-var bounding = videos.getBoundingClientRect();
-
-if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+  if ( bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
     // console.log('Element is in the viewport!');
     videos.play();
-    play_arr.display.style("none");
     // return 1;
-    videos.loop=true;
-} else {
+    videos.loop = true;
+  } else {
     // console.log('Element is NOT in the viewport!');
     videos.pause();
-    play_arr.display.style("block");
     // return 0;
-    videos.loop=false;
-}
+    videos.loop = false;
+  }
 }
 
 // window.addEventListener('resize', checkScroll, false);
 
-window.addEventListener('scroll', newfunc, false);
+window.addEventListener("scroll", newfunc, false);
+
