586a38a15ee2e502a8e69dc61e1d900a447fed1f
Adityagupta1625 <guptaaditya2512@gmail.com>
2021-08-09 10:26:31 +0530

    new database

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-
-import requests
-from os import replace
-import os
-import boto3
+from MySQLdb.cursors import Cursor
 from flask import Flask, render_template, request, redirect
-import firebase_admin
-from firebase_admin import credentials, firestore, initialize_app
 from boto3 import session
-from botocore.client import Config
 from boto3.s3.transfer import S3Transfer
-from requests.api import options
+import mysql.connector
+
 
 ACCESS_ID = '5GFF7YOAHZWTV5VKP7Q6'
 SECRET_KEY = '3mPgnLrUXGhLSb+0JHFElxP0HICyv7LSaEw0hsLWs5c'
@@ -24,151 +18,40 @@ client = session.client('s3',
 transfer = S3Transfer(client)
 # Upload a file to your Space
 
-cred = credentials.Certificate('key.json')
-firebase_admin.initialize_app(cred)
-
-
 
 app = Flask(__name__,template_folder='template')
-# upload_path = 'static/asset/Videos/'
-# app.config['UPLOAD_FOLDER'] = upload_path
 
+conn = mysql.connector.connect(host='127.0.0.1',
+                              database='gimizogames',
+                              user='gimizodata',
+                              password='King04@2021!',
+                              auth_plugin='mysql_native_password')
 
+cursor=conn.cursor()
 @app.route('/')
 def index():
-    db = firestore.client()
-    # docs = db.collection('apps').document('apps').collection('Popular').get()
-
-    # l = []
-    # for doc in docs:
-    #     l.append(doc.to_dict())
-
-    list1 = []
-    cat_refget = db.collection('category').get()
-
-    for c in cat_refget:
-
-        list1.append(c.to_dict())
+    cursor.execute('''SELECT * from games WHERE category="Popular"''')
     
-    a = []
-    for i in list1:
-        for j in i.values():
-            a.append(j)
+    result=cursor.fetchall()
 
-    dc=db.collection('games').document('gamizoapp').collection('Popular').document('0').get()
-    dc_num=dc.to_dict()
+    
+    cursor.execute('''SELECT DISTINCT category from games''')
+    a=cursor.fetchall()
+  
 
-    k=1
-    newlist=[]
-    while k<=dc_num['recordnum']:
-        doc = db.collection('games').document('gamizoapp').collection('Popular').document(f'{k}').get()
-        op=doc.to_dict()
-       
-        for key, value in op.items():
-            if not key == 'numbercount':
-              newlist.append(value)  
-        k=k+1
-    a.sort()
-    return render_template('index.html', list=newlist, cat=a)
+    return render_template('index.html', list=result, cat=a)
 
 
 @app.route('/<string:cat>')
 def index1(cat):
-    # db = firestore.client()
-    # docs = db.collection('apps').document('apps').collection(cat).get()
-
-    # l = []
-    # for doc in docs:
-    #     l.append(doc.to_dict())
-    db=firestore.client()
-    
-    list1 = []
-    cat_refget = db.collection('category').get()
-
-    for c in cat_refget:
-
-        list1.append(c.to_dict())
-    
-    a = []
-    for i in list1:
-        for j in i.values():
-            a.append(j)
-    
+    cursor.execute('''SELECT * from games WHERE category=%s''',(cat))
+    result=cursor.fetchall()
 
 
-    dc=db.collection('games').document('gamizoapp').collection(cat).document('0').get()
-    dc_num=dc.to_dict()
+    cursor.execute('''SELECT DISTINCT category from games''')
+    a=cursor.fetchall()
 
-    k=1
-    newlist=[]
-    while k<=dc_num['recordnum']:
-        doc = db.collection('games').document('gamizoapp').collection(cat).document(f'{k}').get()
-        op=doc.to_dict()
-       
-        for key, value in op.items():
-            if not key == 'numbercount':
-              newlist.append(value)  
-        k=k+1
-    a.sort()
-    return render_template('index.html', list=newlist, cat=a)
-
-
-@app.route('/share/<string:sharestr>')
-def share(sharestr):
-    db = firestore.client()
-    cat = ''
-    name = ''
-    dc_num=''
-    i = 0
-    while sharestr[i] != '_':
-        cat += sharestr[i]
-        i = i+1
-    i = i+1
-    
-    dc_num=sharestr[i]
-    
-    i=i+2
-    while i < len(sharestr):
-        name += sharestr[i]
-        i = i+1
-    
-    sp={}
-    checkname=''.join(e for e in name if e.isalnum())
-    doc = db.collection('games').document('gamizoapp').collection(cat).document(dc_num).get()
-    op=doc.to_dict()
-       
-    for key, value in op.items():
-        if  key == checkname:
-            sp=value  
-    
-    dc=db.collection('games').document('gamizoapp').collection(cat).document('0').get()
-    dc_num=dc.to_dict()
-
-    k=1
-    newlist=[]
-    while k<=dc_num['recordnum']:
-        doc = db.collection('games').document('gamizoapp').collection('Popular').document(f'{k}').get()
-        op=doc.to_dict()
-       
-        for key, value in op.items():
-            if not key == 'numbercount':
-              newlist.append(value)  
-        k=k+1
-    
-
-    list1 = []
-
-    cat_refget = db.collection('category').get()
-
-    for c in cat_refget:
-
-        list1.append(c.to_dict())
-    a = []
-    for i in list1:
-        for j in i.values():
-            a.append(j)
-    a.sort()
-    return render_template('share-hidden.html', cat=a,sp=sp,list=newlist)
+    return render_template('index.html', cat=a,list=result)
 
 
 
@@ -193,31 +76,31 @@ def terms():
 
 @app.route('/report', methods=['GET', 'POST'])
 def report():
-    if request.method == "POST":
-        option = request.form['report']
-        gamename = request.form['gamename']
-        category = request.form['category']
+    # if request.method == "POST":
+    #     option = request.form['report']
+    #     gamename = request.form['gamename']
+    #     category = request.form['category']
 
-    db = firestore.client()
-    doc_ref = db.collection('Report').document(gamename)
-    docs = db.collection('Report').document(gamename).get()
-    doc = docs.to_dict()
-    if doc:
-        num = doc['number']
-        nums = int(num)
-        nums = nums+1
-        doc_ref.update({
-            'number': f'{nums}',
-            f'{nums}': option
+    # db = firestore.client()
+    # doc_ref = db.collection('Report').document(gamename)
+    # docs = db.collection('Report').document(gamename).get()
+    # doc = docs.to_dict()
+    # if doc:
+    #     num = doc['number']
+    #     nums = int(num)
+    #     nums = nums+1
+    #     doc_ref.update({
+    #         'number': f'{nums}',
+    #         f'{nums}': option
 
-        })
-    else:
-        doc_ref.set({
-            'number': '1',
-            '1': option
-        })
+    #     })
+    # else:
+    #     doc_ref.set({
+    #         'number': '1',
+    #         '1': option
+    #     })
 
-    return redirect(f'/{category}')
+    return redirect('/')
 
 
 if __name__ == '__main__':
